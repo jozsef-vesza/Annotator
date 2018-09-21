9fc1349153d4e145ee031a2718bfacf47ecc8328
József Vesza <jozsef.vesza@outlook.com>
2018-09-21 19:21:16 +0200

    Annotate Swift files in the specified project

--- Sources/AnnotatorCore/Annotator.swift
+++ Sources/AnnotatorCore/Annotator.swift
@@ -1,4 +1,9 @@
 import Foundation
+import SwiftSyntax
+
+enum AnnotatorError: Swift.Error {
+    case missingProjectFolder
+}
 
 public final class Annotator {
     private let arguments: [String]
@@ -8,6 +13,30 @@ public final class Annotator {
     }
     
     public func run() throws {
-        print("Hello world")
+        guard arguments.count > 0 else {
+            throw AnnotatorError.missingProjectFolder
+        }
+        
+        let projectFolder = arguments[1]
+        
+        let enumerator = FileManager.default.enumerator(atPath: projectFolder)
+        let annotator = ExtensionAnnotator()
+        
+        enumerator?.allObjects
+            .filter { element in
+                guard let stringValue = element as? String else { return false }
+                return URL(fileURLWithPath: stringValue).pathExtension == "swift"
+            }
+            .forEach { element in
+                let filePath = projectFolder.appending("/\(element)")
+                let url = URL(fileURLWithPath: filePath)
+                do {
+                    let sourceFile = try SyntaxTreeParser.parse(url)
+                    let annotatedText = annotator.visit(sourceFile)
+                    try annotatedText.description.write(toFile: filePath, atomically: true, encoding: .utf8)
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+        }
     }
 }
