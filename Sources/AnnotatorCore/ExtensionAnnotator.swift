76b1544b0fa636e96b575ba8944074aee9b7cf47
József Vesza <jozsef.vesza@outlook.com>
2018-09-26 22:16:06 +0200

    Simplify implementation by only visiting extension declarations.

--- Sources/AnnotatorCore/ExtensionAnnotator.swift
+++ Sources/AnnotatorCore/ExtensionAnnotator.swift
@@ -23,53 +23,53 @@ import SwiftSyntax
 
 final class ExtensionAnnotator: SyntaxRewriter {
     
-    override func visit(_ node: CodeBlockItemSyntax) -> Syntax {
-        guard
-            let extensionDeclaration = ExtensionDeclaration(node: node),
-            let updatedLeadingTrivia = extensionDeclaration.updatedLeadingTrivia else {
-                return node
+    override func visit(_ node: ExtensionDeclSyntax) -> DeclSyntax {
+        return annotateIfNeeded(node)
+    }
+    
+    func annotateIfNeeded(_ node: ExtensionDeclSyntax) -> ExtensionDeclSyntax {
+        guard let protocolName = node.inheritanceClause?.inheritedTypeCollection.first?.description else { return node }
+        guard isAnnotationNeeded(node: node) else { return node }
+        
+        return SyntaxFactory.makeExtensionDecl(attributes: node.attributes,
+                                               modifiers: node.modifiers,
+                                               extensionKeyword: annotate(node.extensionKeyword, for: protocolName),
+                                               extendedType: node.extendedType,
+                                               inheritanceClause: node.inheritanceClause,
+                                               genericWhereClause: node.genericWhereClause,
+                                               members: node.members)
+    }
+    
+    private func annotate(_ extensionKeyword: TokenSyntax, for protocolName: String) -> TokenSyntax {
+        let commentPiece = TriviaPiece.lineComment(annotationForProtocol(named: protocolName))
+        
+        let newLeadingTrivia = extensionKeyword.leadingTrivia.appending(commentPiece).appending(.newlines(1))
+        return SyntaxFactory.makeExtensionKeyword(leadingTrivia: newLeadingTrivia,
+                                                  trailingTrivia: extensionKeyword.trailingTrivia)
+    }
+    
+    private func isAnnotationNeeded(node: ExtensionDeclSyntax) -> Bool {
+        guard let protocolName = node.inheritanceClause?
+            .inheritedTypeCollection
+            .first?
+            .description
+            .trimmingCharacters(in: .whitespacesAndNewlines) else {
+                return false
         }
         
-        let closingBraceIsPresent = extensionDeclaration.closingBraces.contains("}")
-        let trailingTrivia = (extensionDeclaration.trailingTrivia ?? Trivia.zero).appending(.spaces(1))
+        guard let leadingTrivia = node.leadingTrivia else { return true }
         
-        // MARK: - ProtocolName
-        let extensionKeyword = SyntaxFactory.makeExtensionKeyword(
-            leadingTrivia: updatedLeadingTrivia,
-            trailingTrivia: trailingTrivia)
-        
-        // ClassName
-        let typeIdentifier = SyntaxFactory.makeTypeIdentifier(extensionDeclaration.className)
-        let protoName = SyntaxFactory.makeTypeIdentifier(extensionDeclaration.protocolName,
-                                                         leadingTrivia: Trivia.spaces(1),
-                                                         trailingTrivia: Trivia.spaces(1))
-        
-        // ProtocolName
-        let protoIdentifier = SyntaxFactory.makeInheritedTypeList([SyntaxFactory.makeInheritedType(
-            typeName: protoName,
-            trailingComma: nil)])
-        
-        // ClassName: ProtocolName
-        let inheritanceClause = SyntaxFactory.makeTypeInheritanceClause(
-            colon: SyntaxFactory.makeColonToken(),
-            inheritedTypeCollection: protoIdentifier)
-        
-        // {}
-        let emptyMembersList = SyntaxFactory.makeMemberDeclBlock(
-            leftBrace: SyntaxFactory.makeToken(TokenKind.leftBrace, presence: .present),
-            members: SyntaxFactory.makeBlankMemberDeclList(),
-            rightBrace: SyntaxFactory.makeToken(TokenKind.rightBrace, presence: closingBraceIsPresent ? .present : .missing))
-        
-        // extension ClassName: ProtocolName {}
-        let annotated = SyntaxFactory.makeExtensionDecl(
-            attributes: nil,
-            modifiers: nil,
-            extensionKeyword: extensionKeyword,
-            extendedType: typeIdentifier,
-            inheritanceClause: inheritanceClause,
-            genericWhereClause: nil,
-            members: extensionDeclaration.methodDeclarationList ?? emptyMembersList)
-        
-        return annotated
+        return leadingTrivia
+            .filter { (piece) -> Bool in
+                switch piece {
+                case .lineComment(annotationForProtocol(named: protocolName)): return true
+                default: return false
+                }
+            }
+            .count == 0
+    }
+    
+    private func annotationForProtocol(named protocolName: String) -> String {
+        return "// MARK: - \(protocolName)"
     }
 }
